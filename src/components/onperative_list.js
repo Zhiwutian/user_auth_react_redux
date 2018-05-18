88f65e71b269f4104b2fc86945d4106b495165bb
zhiwutian <brettalbright@outlook.com>
2018-05-18 15:32:13 -0700

    completed basic pages

---
+++ src/components/onperative_list.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default props => (
+    <div>
+        <h1 className="center">This is the Secret Operative List</h1>
+        <ul>
+            <li>Jim</li>
+            <li>Pam</li>
+            <li>Lucus</li>
+            <li>Johnny</li>
+        </ul>
+
+    </div>
+)
---
+++ src/components/secret_doc.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default props => (
+    <div>
+        <h1 className="center">This is the Secret Document page page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, laboriosam nihil perspiciatis provident quaerat vitae?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolorum fuga iure obcaecati reprehenderit tenetur unde.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis hic mollitia quis quisquam tempora.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, laboriosam nihil perspiciatis provident quaerat vitae?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolorum fuga iure obcaecati reprehenderit tenetur unde.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis hic mollitia quis quisquam tempora.</p>
+    </div>
+)
