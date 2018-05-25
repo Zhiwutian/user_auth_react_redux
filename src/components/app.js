186e179e2ebfaf78ae6cab10efb8f116283625ac
zhiwutian <brettalbright@outlook.com>
2018-05-24 17:10:00 -0700

    completed user authentication and errors in signup and signout components

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,11 @@ import Home from "./home";
 import About from "./about";
 import SecretDoc from "./secret_doc";
 import OperativesList from "./onperative_list";
+import SignIn from "./sign_in"
+import SignUp from "./sign_up";
+import MovieQuote from "./movie_quote";
 import auth from "../hoc/auth";
+import redirect from "../hoc/redirect";
 
 const App = () => (
     <div>
@@ -16,7 +20,10 @@ const App = () => (
                 <Route exact path="/" component={Home}/>
                 <Route path="/about" component={About}/>
                 <Route path="/secret-doc" component={auth(SecretDoc)}/>
-                <Route path="/operative-list" component={OperativesList}/>
+                <Route path="/operative-list" component={auth(OperativesList)}/>
+                <Route path="/sign-up" component={redirect(SignUp, "/movie-quote")} />
+                <Route path= "/sign-in" component={redirect(SignIn, "/movie-quote")}/>
+                <Route path="/movie-quote" component={auth(MovieQuote)}/>
             </div>
 
         </div>
